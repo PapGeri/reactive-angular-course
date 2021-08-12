8730c58fe21c93f21fe5ff05f898e987b2b173ab
PapGeri <gergo.pap97@gmail.com>
2021-08-12 18:37:13 +0200

    - Add Course Service - Refactored home to reactive mode - Using Observables and async pipe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Course, sortCoursesBySeqNo } from '../model/course';
-import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
 	selector: 'home',
@@ -10,20 +12,25 @@ import { CourseDialogComponent } from '../course-dialog/course-dialog.component'
 	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	beginnerCourses: Course[];
-	advancedCourses: Course[];
+	beginnerCourses$: Observable<Course[]>;
+	advancedCourses$: Observable<Course[]>;
 
-	constructor(private http: HttpClient, private dialog: MatDialog) {
+	constructor(private dialog: MatDialog, private courseService: CoursesService) {
 	}
 
 	ngOnInit() {
-		this.http.get('/api/courses')
-			.subscribe(
-				res => {
-					const courses: Course[] = res['payload'].sort(sortCoursesBySeqNo);
-					this.beginnerCourses = courses.filter(course => course.category == 'BEGINNER');
-					this.advancedCourses = courses.filter(course => course.category == 'ADVANCED');
-				});
+		const courses$: Observable<Course[]> = this.courseService.loadAllCourses()
+			.pipe(
+				map(courses => courses.sort(sortCoursesBySeqNo))
+			);
+		this.beginnerCourses$ = courses$
+			.pipe(
+				map(courses => courses.filter(course => course.category === 'BEGINNER'))
+			);
+		this.advancedCourses$ = courses$
+			.pipe(
+				map(courses => courses.filter(course => course.category === 'ADVANCED'))
+			);
 	}
 
 	editCourse(course: Course) {
